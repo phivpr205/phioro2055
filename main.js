eb52d5fd77dd0a217831466bdb2575b1bd97ac08
phivpr205 <yockkfa824218@hotmail.com>
2025-08-05 18:26:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ moment rose just educate garlic settle talent find cushion wall labor any
 `.trim();
 
 const CONFIG = {
-    rpcEndpoint: "https://testnet-rpc.zigchain.com",
+    rpcEndpoint: "https://public-zigchain-testnet-rpc.numia.xyz",
     chainId: "zig-test-2",
     zigDenom: "uzig",
     oroDenom: "coin.zig10rfjm85jmzfhravjwpq3hcdz8ngxg7lxd0drkr.uoro",
